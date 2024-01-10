5994ebc6705a890739778207c7082d9db16236b7
Your Name <you@example.com>
2024-01-10 20:41:12 +0500

    html bilan ishlash

--- index.js
+++ index.js
@@ -1,12 +1,59 @@
-const http = require('http')
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const server = http.createServer((req, res) => {
+  if (req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "text/html" });
 
-const server = http.createServer((request, response) =>{
-    console.log(request.url);
-    response.write('<h1>hello world 1</h1>')
-    response.write('<h1>hello world 18</h1>')
-    response.end()
-})
+    if (req.url === "/") {
+      fs.readFile(
+        path.join(__dirname, "template", "index.html"),
+        "utf-8",
+        (err, conten) => {
+          if (err) throw err;
+          res.end(conten);
+        }
+      )
+    }
 
-server.listen(3000, () =>{
-    console.log('server has been started port on 3000');
-})
+
+    else if (req.url === "/about") {
+      fs.readFile(
+        path.join(__dirname, "template", "about.html"),
+        "utf-8",
+        (err, conten) => {
+          if (err) throw err;
+          res.end(conten);
+        }
+      )
+    }
+    else if (req.url === "/contact") {
+        fs.readFile(
+          path.join(__dirname, "template", "contact.html"),
+          "utf-8",
+          (err, conten) => {
+            if (err) throw err;
+            res.end(conten);
+          }
+        )
+      }
+
+  }
+  
+  else if (req.method === "POST") {
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+
+    const body = [];
+    req.on("data", (data) => {
+      body.push(Buffer.from(data));
+    });
+    req.on("end", () => {
+      const massage = body.toString().split("=")[1];
+      res.end(`user keldi: ${massage}`);
+    });
+  }
+});
+
+server.listen(3000, () => {
+  console.log("server has been started port on 3000");
+});
